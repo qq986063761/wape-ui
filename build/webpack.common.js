76899a502deab431163a8e59f44dab41e44ac6b9
WanPeng <986063761@qq.com>
2020-10-11 17:36:34 +0800

    更新

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
     filename: 'wape-ui.js',
     libraryExport: 'default',
     library: 'WAPE',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2' // commonjs2 用于 node 环境 es6 引入库；umd 用于浏览器端直接引入库
   },
   module: {
     rules: [
